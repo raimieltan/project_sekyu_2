8ffc663be80a809e06f41fb7f71d2466e1eca5c1
raimieltan <raimieltan@gmail.com>
2022-09-29 08:56:15 +0800

    first commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody bulletRigidbody;
+    public float force;
+    public float torque;
+    private void Awake() {
+        bulletRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start() {
+
+        // bulletRigidbody.velocity = transform.forward * speed;
+        bulletRigidbody.AddForce(transform.forward * force, ForceMode.Impulse);
+        bulletRigidbody.AddTorque(transform.right * torque);
+        transform.SetParent(null);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
