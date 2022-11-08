1e9400627a8be584444b2e5e89310eed61982b50
Michael Handumon <kmcaelian@gmail.com>
2022-11-08 20:12:13 +0800

    fix: script syntax errors

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -31,8 +31,6 @@ public class ThirdPersonShooterController : MonoBehaviour
 
     private Transform currentBullet;
     private string animationTrigger;
-
-    private Animator animator;
     private void Awake()
     {
         thirdPersonController = GetComponent<ThirdPersonController>();
@@ -41,95 +39,86 @@ public class ThirdPersonShooterController : MonoBehaviour
         currentBullet = pfBulletProj;
         animator = GetComponent<Animator>();
         animationTrigger = "Shoot";
-        animator = GetComponent<Animator>();
         canAttack = true;
     }
     private void Update()
     {
         
-        Vector3 mouseWorldPosition = Vector3.zero;
+            Vector3 mouseWorldPosition = Vector3.zero;
 
-        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
-        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
-        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderMask)) {
-          
-            mouseWorldPosition = raycastHit.point;
-        }
-
-        if (Input.GetButtonDown("ToggleRevive")){
-            if(!isReviving) {
-                isReviving = true;
-                currentBullet = reviveOrb;
-                animationTrigger = "Revive";
-
-            } else {
-                isReviving = false;
-                currentBullet = pfBulletProj;
-                animationTrigger = "Shoot";
+            Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+            Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderMask)) {
+            
+                mouseWorldPosition = raycastHit.point;
             }
-        }
-        if(type == AttackType.MAGIC){
-            if(starterAssetsInputs.aim){
-                aimVirtualCamera.gameObject.SetActive(true);
-                thirdPersonController.SetSensitivity(aimSensitivity);
 
-                Vector3 worldAimTarget = mouseWorldPosition;
-                worldAimTarget.y = transform.position.y;
-                Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
-                transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
-            Vector3 worldAimTarget = mouseWorldPosition;
-            worldAimTarget.y = transform.position.y;
-            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
-            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
+            if (Input.GetButtonDown("ToggleRevive")){
+                if(!isReviving) {
+                    isReviving = true;
+                    currentBullet = reviveOrb;
+                    animationTrigger = "Revive";
 
-            if(starterAssetsInputs.shoot) {
-                Vector3 aimDir = (mouseWorldPosition - spawnBulletPos.position).normalized;
-
-                if(starterAssetsInputs.shoot && canAttack) {
-
-                    StartCoroutine(attackMagic(aimDir)); 
-                    
+                } else {
+                    isReviving = false;
+                    currentBullet = pfBulletProj;
+                    animationTrigger = "Shoot";
                 }
             }
+            if(type == AttackType.MAGIC){
+                if(starterAssetsInputs.aim){
+                    aimVirtualCamera.gameObject.SetActive(true);
+                    thirdPersonController.SetSensitivity(aimSensitivity);
+                    Vector3 worldAimTarget = mouseWorldPosition;
+                    worldAimTarget.y = transform.position.y;
+                    Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+                    transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
+
+                    Vector3 aimDir = (mouseWorldPosition - spawnBulletPos.position).normalized;
+
+                    if(starterAssetsInputs.shoot) {
+                        
+                        if(starterAssetsInputs.shoot && canAttack) {
+                            StartCoroutine(attackMagic(aimDir)); 
+                        }
+                    }
+                    else {
+                        aimVirtualCamera.gameObject.SetActive(false);
+                        thirdPersonController.SetSensitivity(normalSensitivity);
+                        Instantiate(currentBullet, spawnBulletPos.position, Quaternion.LookRotation(aimDir, Vector3.up));
+                        starterAssetsInputs.shoot = false;
+                        animator.SetTrigger(animationTrigger);
+                }
+
+                }
             else {
-                aimVirtualCamera.gameObject.SetActive(false);
-                thirdPersonController.SetSensitivity(normalSensitivity);
-                Instantiate(currentBullet, spawnBulletPos.position, Quaternion.LookRotation(aimDir, Vector3.up));
-                starterAssetsInputs.shoot = false;
-                animator.SetTrigger(animationTrigger);
+                if(starterAssetsInputs.shoot && canAttack) {
+                    StartCoroutine(attackMelee());
+                }
             }
 
-        }
-        else {
-            if(starterAssetsInputs.shoot && canAttack) {
-                StartCoroutine(attackMelee());
             }
+
+        IEnumerator attackMelee() {
+            canAttack = false;
+
+            animator.Play("Attack", 0, 0.25f);
+            yield return new WaitForSeconds(1.3f);
+
+            canAttack = true;
+
         }
 
+        IEnumerator attackMagic(Vector3 aimDir) {
+            canAttack = false;
+
+            animator.Play("SpellAttack", 0, 0.15f);
+            yield return new WaitForSeconds(0.3f);
+
+            Instantiate(pfBulletProj, spawnBulletPos.position, Quaternion.LookRotation(aimDir, Vector3.up));
+            starterAssetsInputs.shoot = false;
+
+            canAttack = true;
+        }
     }
-
-    IEnumerator attackMelee() {
-        canAttack = false;
-
-        animator.Play("Attack", 0, 0.25f);
-        yield return new WaitForSeconds(1.3f);
-
-        canAttack = true;
-
-    }
-
-    IEnumerator attackMagic(Vector3 aimDir) {
-        canAttack = false;
-
-        animator.Play("SpellAttack", 0, 0.15f);
-        yield return new WaitForSeconds(0.3f);
-
-        Instantiate(pfBulletProj, spawnBulletPos.position, Quaternion.LookRotation(aimDir, Vector3.up));
-        starterAssetsInputs.shoot = false;
-     
-
-        canAttack = true;
-
-    }
-
 }
