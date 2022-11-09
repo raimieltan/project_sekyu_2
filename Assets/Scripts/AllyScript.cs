75b6b38d329ce8b6834c9ad473ebc10a598e8ddc
Michael Handumon <kmcaelian@gmail.com>
2022-11-09 16:15:06 +0800

    fix: dead body collider issue

--- Assets/Scripts/AllyScript.cs
+++ Assets/Scripts/AllyScript.cs
@@ -8,12 +8,19 @@ public class AllyScript : MonoBehaviour
     private Animator animator;
     private int health;
     private bool isDead;
+    private Transform root;
+    private CapsuleCollider collider;
+
+    private Vector3 center;
 
     private void Start() {
         health = 100;
         isDead = false;
         animator = GetComponent<Animator>();
+        collider = GetComponent<CapsuleCollider>();
+        center = collider.center;
     }
+
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.tag == "bullet" && !isDead)
         {
@@ -22,6 +29,12 @@ public class AllyScript : MonoBehaviour
             if (health < 0) {
                 isDead = true;
                 animator.SetTrigger("isDead");
+                collider.direction = 2;
+                Vector3 center = collider.center;
+                center.y = 0.5f;
+                center.z = -0.5f;
+                
+                collider.center = center;
             }
         }
         else if (other.gameObject.tag == "revive" && isDead)
@@ -30,6 +43,12 @@ public class AllyScript : MonoBehaviour
             isDead = false;
             animator.SetTrigger("isRevive");
             animator.ResetTrigger("isDead");
+            collider.direction = 1;
+
+            center.y = 1f;
+            center.z = 0f;
+                
+            collider.center = center;
         }
         Debug.Log(isDead);
     }
