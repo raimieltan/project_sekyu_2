967d5110f1283453e98a13ba4dcbdb64bf621763
Michael Handumon <kmcaelian@gmail.com>
2022-11-25 14:29:02 +0800

    feat: input manager changes

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6eaaed3a5aceb59ba5559b06bf61c40c3c303b4bccd85ffa8c831f01a4d27654
-size 1139
+oid sha256:a9f50e4453dc01fe9a995a467c822d36dd2b0e26a4668a56a159a81a41227fa4
+size 1067
