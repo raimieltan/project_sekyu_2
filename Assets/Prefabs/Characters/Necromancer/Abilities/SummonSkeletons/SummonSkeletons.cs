c795f39bc66d30aae8e9ea51d45c2c3449353a39
Michael Handumon <kmcaelian@gmail.com>
2022-11-28 17:40:32 +0800

    feat: convert keydown ability to starterinput ability

--- Assets/Prefabs/Characters/Necromancer/Abilities/SummonSkeletons/SummonSkeletons.cs
+++ Assets/Prefabs/Characters/Necromancer/Abilities/SummonSkeletons/SummonSkeletons.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6924c5fe2b101d18af48b535969df75bad4d7b79cffce34f455ffcf8a0778e28
-size 1732
+oid sha256:8ca989b3f838b01ae6c8b9ed851d74af9f486df4046a2927a5db4e8f04897cb2
+size 1848
