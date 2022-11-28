ba0e66ec98bdcee56cac4fe5cc9fae1a6f610369
Michael Handumon <kmcaelian@gmail.com>
2022-11-28 19:24:41 +0800

    feat: toggle revive ability using starterInputs

--- Assets/Prefabs/Characters/Necromancer/Abilities/Revive/Revive.cs
+++ Assets/Prefabs/Characters/Necromancer/Abilities/Revive/Revive.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af0ed8a231867bc60a178d4d7ba63673fe68d670b2df064a51ab6e91bb640dba
-size 946
+oid sha256:2950bde1d67af9e99b9f0ba0a39ed366bced6cc4bf101ac013afb2cfaedbc1ae
+size 3326
--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5d42def3ac299f9c1f7bfe77cf4df7b1556f8d824ccc485d007f0e58a00849a
-size 3224
+oid sha256:2537b594ae909289bc9c5fca5c27d7ff242234ca04bf3006d1ca115df4a3ed56
+size 3258
