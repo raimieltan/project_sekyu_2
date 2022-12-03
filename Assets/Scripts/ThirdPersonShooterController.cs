12433e78c54685f5cf0c31aa5afdaa3e05ac85ad
Aftertaste26 <20hardrock12@gmail.com>
2022-12-03 14:49:45 +0800

    feat: added heal powerup

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47bdded0353e6872c77890d60e4cf567ee9363f2110dce2f3eb2bf127a70aee3
-size 4321
+oid sha256:08b4250f2ba9e8b7978e7e29fc0effdacfba6bff674321cf7bc5469169e7c36f
+size 4354
