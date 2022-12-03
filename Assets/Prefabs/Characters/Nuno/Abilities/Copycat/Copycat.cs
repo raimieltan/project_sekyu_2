b1f970a2a47a549f9f3cdd09c40f8e819a8a0c83
Michael Handumon <kmcaelian@gmail.com>
2022-12-03 20:58:28 +0800

    feat: nuno invisibility duration time

--- Assets/Prefabs/Characters/Nuno/Abilities/Copycat/Copycat.cs
+++ Assets/Prefabs/Characters/Nuno/Abilities/Copycat/Copycat.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9aeb4e98343e88af6c0d609e057ae162dd2e73cdcb58a73bfc82a92fe031536d
-size 2033
+oid sha256:29d96a756fddde6f7f90853d3bf18d6873b1842122a8686776e84b7cf2d0efa8
+size 2183
--- Assets/Prefabs/Characters/Nuno/Abilities/Invisibility/Invisibility.cs
+++ Assets/Prefabs/Characters/Nuno/Abilities/Invisibility/Invisibility.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56b0a34353c55f1e54561a8ad2b01321eb36c792ac0804a27ae00fa1d22591c0
-size 3727
+oid sha256:206151e975ebb6d8105f5ec664c8e2b4f19ced972bb8f4f04d7c756ce5cf6656
+size 3624
