f99d4943dd9e473b8ecf5e2beb7f2b353b9dd512
Veto Bastiero <vetobastiero@gmail.com>
2022-12-04 12:38:36 +0800

    feat: adds nuno hud

--- Assets/Prefabs/Characters/Nuno/Abilities/Invisibility/Invisibility.cs
+++ Assets/Prefabs/Characters/Nuno/Abilities/Invisibility/Invisibility.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa57b501011d221742821ed2fe40c182c09e36bab1bebca7855a01dcb1630ba2
-size 3774
+oid sha256:d2e29f4e33856243ceb842734dd65f0234dae14f4c8c229491aede56bd672408
+size 3735
