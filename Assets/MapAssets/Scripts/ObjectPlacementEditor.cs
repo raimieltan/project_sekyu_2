a33d5ed34edbe727c383e7d6d0140c3f5c19d9f0
Michael Handumon <kmcaelian@gmail.com>
2022-12-05 16:13:31 +0800

    feat: added procedural powerup spawning in game map

--- Assets/MapAssets/Scripts/ObjectPlacementEditor.cs
+++ Assets/MapAssets/Scripts/ObjectPlacementEditor.cs
@@ -1,23 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-
-[CustomEditor(typeof(ObjectPlacement))]
-public class ObjectPlacementEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
-
-        ObjectPlacement script = (ObjectPlacement)target;
-        if(GUILayout.Button("Generate"))
-        {
-            script.Generate();
-        }
-        if(GUILayout.Button("Clear"))
-        {
-            script.Clear();
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:0a68c4232b1dc27e930ccd4c6a7a9c792b4733b3bf61013766b8749af02fc8a6
+size 540
