cd468f924f40fcf76e311e024131f7d5d8af94dc
Casey Rhyss Capacio <capaciocasey@gmail.com>
2022-12-05 14:32:46 +0800

    chore: remove debug

--- Assets/Scripts/MapAreaCollider.cs
+++ Assets/Scripts/MapAreaCollider.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5580fc1eda071fa28b4240bbecd8448f64257d643ae277cd04e43e109212b5bb
-size 2546
+oid sha256:f125ea629c9a57c36a35dff48f2acc3409b11dbb666c185e10a52c401f3839a3
+size 2549
