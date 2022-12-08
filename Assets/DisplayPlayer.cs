6ab3b9bc5a2da298efa75e79cca3171c31358e7c
raimieltan <raimieltan@gmail.com>
2022-12-08 16:26:58 +0800

    fix: UI integration

--- Assets/DisplayPlayer.cs
+++ Assets/DisplayPlayer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55d321aa7a18cce26ce54d35ced6bd40fdaf7ee17acad9fbd566f8fcfe01c5f8
+oid sha256:45f0d1f3f227a07f64af660e2d1f6f3deb23ac2d4953567e6d20f05257578022
 size 1286
--- Assets/MapAssets/Scripts/ObjectPlacement.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a379b3db8a6b0828e23a786467efd9c92a31d463494c339081926e5d6dd133c0
-size 8594
