93253cb8db0ebb8e13d17d237a0f7abc5a339ee5
Casey Rhyss Capacio <capaciocasey@gmail.com>
2022-12-08 18:18:23 +0800

    fix: fixed tikbalang script

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2537b594ae909289bc9c5fca5c27d7ff242234ca04bf3006d1ca115df4a3ed56
-size 3258
+oid sha256:2a19c1eeaa1b40933d40b89c9d855e06125c14b1d7fa5fcf2b2643acaf4b7752
+size 3841
