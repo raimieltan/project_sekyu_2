6103adbd7fef9172cc0804ea87828322c2c8fe4c
raimieltan <raimieltan@gmail.com>
2022-12-11 23:08:36 +0800

    feat: damage from weapons with playerhud

--- Assets/UI/HUD/Scripts/HudManager.cs
+++ Assets/UI/HUD/Scripts/HudManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7240ecd88785cd94bb6ab2c510811fc0f30c991c69ff69d1b7ea261a4dd2498c
-size 4787
+oid sha256:ca9c02dde96afd60fa8df27a2a128cc29f3f6c145cee7ed8200b5a1b0cd17b57
+size 4675
