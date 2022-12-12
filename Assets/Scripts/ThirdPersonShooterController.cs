fb2ef674eda7ee2171b0cdb5bbd5475bd5917926
Alejandro Jonela <ajonela46@gmail.com>
2022-12-12 14:37:53 +0800

    fix: fix multiplayer map assets

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fefad2e5526bf7418d2a49642645ab07df3fb97e5d4fd1c4a573a55d26ebf17c
-size 3896
+oid sha256:b2a1dc1e798ea89e7120c3903e0302154d347fae8aecf603a12819c354abe985
+size 4481
