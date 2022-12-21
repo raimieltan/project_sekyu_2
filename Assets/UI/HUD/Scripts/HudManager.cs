1e6546f277cbeec5833aa662582ba7b7b2ecbf43
Veto Bastiero <72717249+veto24@users.noreply.github.com>
2022-12-21 11:48:44 +0800

    Hud improvements (enlarge images and adds cooldown text) (#52)
    * feat: enlarge images and adds cooldown text
    
    * feat: apply updating cooldown text

--- Assets/UI/HUD/Scripts/HudManager.cs
+++ Assets/UI/HUD/Scripts/HudManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca9c02dde96afd60fa8df27a2a128cc29f3f6c145cee7ed8200b5a1b0cd17b57
-size 4675
+oid sha256:d935971f51b35cf3853437ff95ca6c1761aba15565b33b5565fd5a70711525d9
+size 5360
