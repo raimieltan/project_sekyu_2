7508c83e061f5b6df7ab52b2c53c256727b02119
Kent Michael Handumon <70324143+michaelhandumon@users.noreply.github.com>
2023-02-11 19:11:19 +0800

    Encanto new ability (#58)
    * feat: nuno characters with melee weapon
    
    * feat: nuno character rework
    
    * feat: compile encanto scripts
    
    * feat: added chooseEncanto function on display player script
    
    * lfs changes
    
    * feat: created stun script
    
    * feat: compile encanto for multiplayer scene
    
    * chore: modify heal script and bullet collider
    
    * lfs changes
    
    * feat: encanto multiplayer playable
    
    * chore: minor code cleanup
    
    * feat: refactor stun and heal abilities

---
+++ Assets/Prefabs/Characters/Encanto/Abilities/AoEStun/Stun.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c5842229e6113a1b85bf1d90ab3292d66d497994b7046c87127869a94a58d54
+size 2699
--- Assets/Prefabs/Characters/Encanto/Abilities/AoeHeal/Heal.cs
+++ Assets/Prefabs/Characters/Encanto/Abilities/AoeHeal/Heal.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:373850e78bb1767de6bb79b0b7caad99f898521a6a70d68ccd0187a421f2c4f7
-size 2249
+oid sha256:6eb9d3e3a66d531af2db385d46655b210eb622ea800f9e89ffb84a95a1b50cae
+size 2345
