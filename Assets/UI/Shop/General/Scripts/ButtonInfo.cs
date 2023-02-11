1626b9f290cfb8be52826aa11ebb7daf2de44e64
Alejandro Jonela <58468148+Thevalk1@users.noreply.github.com>
2023-02-11 19:33:31 +0800

    Connect inventory to item player item HUD (#59)
    * chore: remove inventory inside each character
    
    * feat: add inventory shop in game scene
    
    * chore: update shopmanager script
    
    * chore: update buy button
    
    * chore: event system prefab
    
    * chore: update shop prefab
    
    * chore: not show item hud when not bought
    
    * feat: connect inventory to hud
    
    * chore: lfs
    
    ---------
    
    Co-authored-by: raimieltan <54674698+raimieltan@users.noreply.github.com>

--- Assets/UI/Shop/General/Scripts/ButtonInfo.cs
+++ Assets/UI/Shop/General/Scripts/ButtonInfo.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfc9fb1d1e62ee2457abd60663348df24eda232be2b332524fbdda0da0efcc16
-size 714
+oid sha256:024e65e12b37e51f937767e597406a0fd233a96b7230e430d39808684d184c99
+size 872
--- Assets/UI/Shop/General/Scripts/ShopManagerScript.cs
+++ Assets/UI/Shop/General/Scripts/ShopManagerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:403f4c2460b4bfc3b9c6b40427e557e83108eb99cd92053fdc7c70fd3ec4ecfb
-size 3193
+oid sha256:db797ffbbb63d326537003ba9c1a1d92d7eed0788f52485f3f656e03eda53ecc
+size 3229
