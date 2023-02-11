6f8de5d0f4cb34b09704522b5b3f3562c42add60
Alejandro Jonela <58468148+Thevalk1@users.noreply.github.com>
2023-02-12 03:28:37 +0800

    PlayerHud Item quantity fix (#62)
    * fix: Hud item quantity
    
    * chore: update character playerhud

--- Assets/UI/Shop/General/Scripts/OpenShop.cs
+++ Assets/UI/Shop/General/Scripts/OpenShop.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b22b53de0ebbd873a31112e413839c530f7ca1b6e13ae51b308426e2d0b6982
-size 1264
+oid sha256:5b421e4591e8f855bdc2c9dc9a46ba6531bcd674c35add65fe912df327f5e9eb
+size 808
--- Assets/UI/Shop/General/Scripts/ShopManagerScript.cs
+++ Assets/UI/Shop/General/Scripts/ShopManagerScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db797ffbbb63d326537003ba9c1a1d92d7eed0788f52485f3f656e03eda53ecc
-size 3229
+oid sha256:0a477b38cc680383f305406855403bcc0c94b5a144cdf31ed8ce7081f32bac88
+size 4385
